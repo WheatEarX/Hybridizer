4f934bc0ae30f5e27d9bd64a85cdf71a5be531c0
WheatEar <wheat_ear@outlook.com>
2024-06-22 11:23:30 +0800

    Features On

--- src/main/java/com/wheat_ear/worldgen/ModConfiguredFeatures.java
+++ src/main/java/com/wheat_ear/worldgen/ModConfiguredFeatures.java
@@ -1,15 +1,22 @@
 package com.wheat_ear.worldgen;
 
+import com.wheat_ear.Hybridizer;
 import net.minecraft.registry.Registerable;
 import net.minecraft.registry.RegistryKey;
+import net.minecraft.registry.RegistryKeys;
+import net.minecraft.util.Identifier;
 import net.minecraft.world.gen.feature.ConfiguredFeature;
 import net.minecraft.world.gen.feature.ConfiguredFeatures;
 import net.minecraft.world.gen.feature.Feature;
 
 public class ModConfiguredFeatures {
-    public static final RegistryKey<ConfiguredFeature<?, ?>> OAK_BIRCH = ConfiguredFeatures.of("oak_birch");
+    public static final RegistryKey<ConfiguredFeature<?, ?>> OAK_BIRCH = of("oak_birch");
 
     public static void bootstrap(Registerable<ConfiguredFeature<?, ?>> featureRegisterable) {
         ConfiguredFeatures.register(featureRegisterable, OAK_BIRCH, Feature.TREE, ModFeatureConfigs.OAK_BIRCH);
+    }
+
+    public static RegistryKey<ConfiguredFeature<?, ?>> of(String id) {
+        return RegistryKey.of(RegistryKeys.CONFIGURED_FEATURE, new Identifier(Hybridizer.MOD_ID, id));
     }
 }
--- src/main/java/com/wheat_ear/worldgen/ModPlacedFeatures.java
+++ src/main/java/com/wheat_ear/worldgen/ModPlacedFeatures.java
@@ -1,20 +1,26 @@
 package com.wheat_ear.worldgen;
 
+import com.wheat_ear.Hybridizer;
 import com.wheat_ear.block.ModBlocks;
 import net.minecraft.registry.Registerable;
 import net.minecraft.registry.RegistryEntryLookup;
 import net.minecraft.registry.RegistryKey;
 import net.minecraft.registry.RegistryKeys;
+import net.minecraft.util.Identifier;
 import net.minecraft.world.gen.feature.ConfiguredFeature;
 import net.minecraft.world.gen.feature.PlacedFeature;
 import net.minecraft.world.gen.feature.PlacedFeatures;
 
 public class ModPlacedFeatures {
-    public static final RegistryKey<PlacedFeature> OAK_BIRCH = PlacedFeatures.of("oak_birch");
+    public static final RegistryKey<PlacedFeature> OAK_BIRCH = of("oak_birch");
 
     public static void bootstrap(Registerable<PlacedFeature> featureRegisterable) {
         RegistryEntryLookup<ConfiguredFeature<?, ?>> registryEntryLookup = featureRegisterable.getRegistryLookup(RegistryKeys.CONFIGURED_FEATURE);
         PlacedFeatures.register(featureRegisterable, OAK_BIRCH, registryEntryLookup.getOrThrow(ModConfiguredFeatures.OAK_BIRCH),
                 PlacedFeatures.wouldSurvive(ModBlocks.OAK_BIRCH_SAPLING));
+    }
+
+    public static RegistryKey<PlacedFeature> of(String id) {
+        return RegistryKey.of(RegistryKeys.PLACED_FEATURE, new Identifier(Hybridizer.MOD_ID, id));
     }
 }
